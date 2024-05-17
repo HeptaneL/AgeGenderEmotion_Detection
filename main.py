2b4d2b77c65d12dc741feb21ea575ed56da718d3
HeptaneL <1010410164@qq.com>
2024-05-17 15:17:50 +0800

    init

---
+++ main.py
@@ -0,0 +1,76 @@
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+import cv2
+
+#the following are to do with this interactive notebook code
+
+from matplotlib import pyplot as plt # this lets you draw inline pictures in the notebooks
+#%matplotlib inline 
+import pylab # this allows you to control figure size 
+pylab.rcParams['figure.figsize'] = (10.0, 8.0) # this controls figure size in the notebook
+
+export_dir='./age_model_pretrained.h5'
+age_model = load_model(export_dir)
+
+# summarize model.
+age_model.summary()
+
+# load and evaluate a saved model
+export_dir='./gender_model_pretrained.h5'
+gender_model = load_model(export_dir)
+
+# summarize model.
+gender_model.summary()
+
+export_dir='./emotion_model_pretrained.h5'
+emotion_model = load_model(export_dir)
+
+# summarize model.
+emotion_model.summary()
+
+# Labels on Age, Gender and Emotion to be predicted
+
+age_ranges = ['1-2', '3-9', '10-20', '21-27', '28-45', '46-65', '66-116']
+gender_ranges = ['male', 'female']
+emotion_ranges= ['positive','negative','neutral']
+img_path = "./images/family.jpg"
+from IPython.display import Image
+pil_img = Image(filename=img_path)
+display(pil_img)
+test_image = cv2.imread(img_path)
+gray = cv2.cvtColor(test_image,cv2.COLOR_BGR2GRAY)
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+i = 0
+
+for (x,y,w,h) in faces:
+  i = i+1
+  cv2.rectangle(test_image,(x,y),(x+w,y+h),(203,12,255),2)
+
+  img_gray=gray[y:y+h,x:x+w]
+
+  emotion_img = cv2.resize(img_gray, (48, 48), interpolation = cv2.INTER_AREA)
+  emotion_image_array = np.array(emotion_img)
+  emotion_input = np.expand_dims(emotion_image_array, axis=0)
+  output_emotion= emotion_ranges[np.argmax(emotion_model.predict(emotion_input))]
+
+  gender_img = cv2.resize(img_gray, (100, 100), interpolation = cv2.INTER_AREA)
+  gender_image_array = np.array(gender_img)
+  gender_input = np.expand_dims(gender_image_array, axis=0)
+  output_gender=gender_ranges[np.argmax(gender_model.predict(gender_input))]
+
+  age_image=cv2.resize(img_gray, (200, 200), interpolation = cv2.INTER_AREA)
+  age_input = age_image.reshape(-1, 200, 200, 1)
+  output_age = age_ranges[np.argmax(age_model.predict(age_input))]
+
+
+  output_str = str(i) + ": "+  output_gender + ', '+ output_age + ', '+ output_emotion
+  print(output_str)
+
+  col = (0,255,0)
+
+  cv2.putText(test_image, str(i),(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,col,2)
+
+plt.imshow(cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB))
